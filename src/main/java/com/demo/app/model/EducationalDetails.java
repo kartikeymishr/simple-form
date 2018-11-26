17d00f1da534e636c946d43b23210437f24c37be
IBMADMIN <IBMADMIN@DESKTOP-9D6MJPL.yellowdc.com>
2018-11-26 16:36:58 +0530

    initial commit

---
+++ src/main/java/com/demo/app/model/EducationalDetails.java
@@ -0,0 +1,23 @@
+package com.demo.app.model;
+
+public class EducationalDetails {
+    private String highestQualification;
+    private float percentage;
+
+    public String getHighestQualification() {
+        return highestQualification;
+    }
+
+    public void setHighestQualification(String highestQualification) {
+        this.highestQualification = highestQualification;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+}
